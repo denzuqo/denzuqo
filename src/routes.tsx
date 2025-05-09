fd8de79ae6f6fc477cd464d9a08a3279a0d94d62
root <denzuqo.com@gmail.com>
2025-05-08 23:03:02 -0500

    20250509 0.0.00022

--- src/routes.tsx
+++ src/routes.tsx
@@ -7,7 +7,6 @@ import ZoraAirdropChecker from './pages/web3/airdrop/checker/ZoraAirdropChecker'
 
 const rawRoutes = [
   { path: '/', element: <App /> },
-  { path: '/.well-known/atproto-did', element: <div>did:plc:52q5abysaaku2u73euojjjgb</div> },
   { path: '/tools/aes', element: <AESTools /> },
   { path: '/sl/:code', element: <ShortLInk /> },
   { path: '/web3/airdrop/checker/zora', element: <ZoraAirdropChecker /> },
